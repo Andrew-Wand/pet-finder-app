31a8f137dab82a1cad3818221d5d785042fd4dd8
Andrew Wand <andrewwands@gmail.com>
2022-12-25 10:58:59 -0600

    pet photo carousel css and options

--- src/components/pages/Animal.jsx
+++ src/components/pages/Animal.jsx
@@ -21,15 +21,16 @@ function Animal() {
 
   const flickityOptions = {
     initialIndex: 0,
+    wrapAround: true,
   };
 
   return (
     <div>
       <h1>{animal.animal?.name}</h1>
 
-      <Flickity flickityOptions={flickityOptions}>
+      <Flickity options={flickityOptions}>
         {animal.animal?.photos.map((item, index) => (
-          <img className="petCarousel-body-slide" src={item.medium} alt="" />
+          <img className="petCarousel-body-slide" src={item.large} alt="" />
         ))}
       </Flickity>
 
